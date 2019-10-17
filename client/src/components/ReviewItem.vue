80ff1557172b9c93b60a0dab52e4dd82ebb78ada
Amir <onikoyiamir@yahoo.com>
2019-10-17 22:45:04 +0200

    Review fixed

--- client/src/components/ReviewItem.vue
+++ client/src/components/ReviewItem.vue
@@ -1,23 +1,48 @@
 <template>
-  <div>
-    <blockquote>
-      <i>" {{review.comment}} " </i>
-      <br>
-      <p id="rating">Rating: <span v-for="rating in review.rating">⭐</span></p>
-      <h6>-{{review.reviewer.fName}} {{review.reviewer.lName}}</h6>
-    </blockquote>
-  </div>
+  <div id="review">
+<blockquote>
+    <i>"{{review.comment}}" </i>
+    <br>
+    <p id="rating">Rating: <span v-for="review in review.rating" :key="review._id">⭐</span></p>
+    <h6>-{{review.reviewer.fName}} {{review.reviewer.lName}}</h6>
+  </blockquote>
 
+
+
+
+
+
+
+
+
+  </div>
 </template>
 
 <script>
+
+
 
     export default {
         name: 'review-item',
         props: ['review']
     }
+
+
 </script>
 
 <style scoped>
 
+  #review{
+    color: #3c3d47;
+  }
+  img{
+    border-radius: 100%;
+    width: 60px;
+  }
+  i{
+    color: black;
+  }
+  #rating{
+    font-weight: bold;
+  }
 </style>
